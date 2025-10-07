041bca79e268b12fa7d90af6a47bb87460929e50
ARTHUR DE OLIVEIRA ROCHA <85263133049@senacrs.edu.br>
2025-10-07 11:23:48 -0300

    updjs

---
+++ 102_frontend/JavaScript/ExArray2.js
@@ -0,0 +1,13 @@
+let numeros = [1, 2, 3]
+console.log(numeros);
+numeros.push (6)
+
+console.log(numeros);
+
+numeros.shift (1);
+console.log(numeros);
+
+numeros.unshift(-3);
+console.log(numeros)
+
+console.log(numeros[3]);
