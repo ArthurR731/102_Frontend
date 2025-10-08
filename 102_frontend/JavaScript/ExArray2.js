15a5e1171bd385e388a8374554e28dbd083af3ea
ARTHUR DE OLIVEIRA ROCHA <85263133049@senacrs.edu.br>
2025-10-08 12:21:30 -0300

    upd

--- 102_frontend/JavaScript/ExArray2.js
+++ 102_frontend/JavaScript/ExArray2.js
@@ -11,3 +11,21 @@ numeros.unshift(-3);
 console.log(numeros)
 
 console.log(numeros[3]);
+
+//=====================================================================
+
+let garage = ["BYD", "Tesla", "Dodge", "Audi", "Lamborghini", "Jaguar", "Subaru", "Pagani", "Mazda"]
+console.log(garage[6])
+
+garage.push("Toyota")
+
+console.log("tamanho do array", garage.length)
+
+for (let i = 0; i < garage.length; i++) {
+    console.log("Carros nº %d:", i+1)
+    console.log("Marca: %s", garage[i])
+}
+
+for (carro in garage) {
+    console.log(carro in garage);
+    }
